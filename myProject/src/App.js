88c5df317e78b6b68ef082d2d4fc226a0e5de3f9
unknown <oloweayodele09@gmail.com>
2023-08-12 08:59:01 +0100

    feat:Hero section

---
+++ myProject/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from './Component/Hero'
+// import Logo from './Component/Logo'
+// import Main from './Component/Main'
+// import Section from './Component/Section'
+// import Footer from './Component/Footer'
+function App() {
+  return (
+    <div>
+      <Hero/>
+      {/* <Logo/>
+      <Main/>
+      <Section/>
+      <Footer/> */}
+    </div>
+  )
+}
+
+export default App
+
